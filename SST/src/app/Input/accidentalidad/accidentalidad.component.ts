46300484b4df31b7539f5ce4db75bc119d155359
Jhuban Rodriguez Vasquez <jhuban.rodriguez@pgrchia.com>
2021-05-06 21:59:40 -0500

    creación de modulo accidentalidad

---
+++ SST/src/app/Input/accidentalidad/accidentalidad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accidentalidad',
+  templateUrl: './accidentalidad.component.html',
+  styleUrls: ['./accidentalidad.component.css']
+})
+export class AccidentalidadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- SST/src/app/app.component.ts
+++ SST/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {AccidentalidadComponent} from './Input/accidentalidad/accidentalidad.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
