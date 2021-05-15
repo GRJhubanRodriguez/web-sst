2c7897cd4fe6fd8d41459d4bf59ff31a02e1c9a9
Jhuban Rodriguez Vasquez <jhuban.rodriguez@pgrchia.com>
2021-05-14 21:27:52 -0500

    Estructura BD
    estructura interna de las tablas usuario y accidentes

---
+++ SST/users_database_sst.sql
@@ -0,0 +1,12 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+DROP TABLE IF EXISTS users;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `enterprise` varchar(7) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
